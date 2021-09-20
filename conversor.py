e6055a4bade68083eee24ad5ba212cd6b499965d
dani <iamomardude@gmail.com>
2021-09-20 03:10:05 -0500

    agregue funciones a la rama pruebas

--- conversor.py
+++ conversor.py
@@ -1,3 +1,12 @@
+def conversor(tipo_dato, valor_dolar):
+    pesos = input('Cuantos pesos' + tipo_dato + 'tienes?: ')
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' '+ 'dolares')
+
+
 menu = """
 Bienvenido al el conversor de moneda
 
@@ -12,35 +21,23 @@ Elige una opcion:
 opcion = input(menu)
 
 if opcion == '1':
-    pesos = input('Cuantos pesos colombianos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' '+ 'dolares')
+    conversor('colombianos', 3875)
 elif opcion == '2':
-    pesos = input('Cuantos pesos argentinos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' '+ 'dolares')
+    conversor('argentinos', 65)
 elif opcion == '3':
-    pesos = input('Cuantos pesos mexicanos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' '+ 'dolares')
+    conversor('mexicanos', 24)
 else:
     print('Ingresa una opcion correcta')
 
 
 
+# def suma(a, b):
+#     print('La suma de los dos numeros es: ')
+#     resultado = a + b
+#     return resultado
 
+# sumatoria = suma(2, 4)
+# print(sumatoria)
 
 
 
